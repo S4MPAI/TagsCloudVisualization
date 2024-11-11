8f33a8d8a4d8dc5aff42f55a6302c0469b0477b9
S4MPAI <flekshum@gmail.com>
2024-11-11 21:11:34 +0500

    DefaultVisualizer.cs: take out parameter Random in readonly field and rename constants in style

--- cs/TagsCloudVisualization/Visualizers/DefaultVisualizer.cs
+++ cs/TagsCloudVisualization/Visualizers/DefaultVisualizer.cs
@@ -4,31 +4,31 @@ namespace TagsCloudVisualization.Visualizers;
 
 public class DefaultVisualizer : IVisualizer
 {
-    private const int minColorComponentValue = 0;
-    private const int maxColorComponentValue = 255;
+    private const int MinColorComponentValue = 0;
+    private const int MaxColorComponentValue = 255;
+    private readonly Random random = new();
     
     public Bitmap CreateBitmap(IList<Rectangle> rectangles, Size bitmapSize)
     {
         var bitmap = new Bitmap(bitmapSize.Width, bitmapSize.Height);
         var graphics = Graphics.FromImage(bitmap);
-        var random = new Random();
         
         foreach (var rectangle in rectangles)
         {
-            var pen = GetRandomPen(random);
+            var pen = GetRandomPen();
             graphics.DrawRectangle(pen, rectangle);
         }
         
         return bitmap;
     }
 
-    private static Pen GetRandomPen(Random random)
+    private Pen GetRandomPen()
     {
         var color = Color.FromArgb
         (
-            random.Next(minColorComponentValue, maxColorComponentValue), 
-            random.Next(minColorComponentValue, maxColorComponentValue), 
-            random.Next(minColorComponentValue, maxColorComponentValue)
+            random.Next(MinColorComponentValue, MaxColorComponentValue), 
+            random.Next(MinColorComponentValue, MaxColorComponentValue), 
+            random.Next(MinColorComponentValue, MaxColorComponentValue)
         );
         
         return new Pen(color);
