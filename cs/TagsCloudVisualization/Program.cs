7b180b52c159e86029027a0a18f40f7b0abb88eb
S4MPAI <flekshum@gmail.com>
2024-11-11 22:33:41 +0500

    fix constants naming in Program and rewrite cycle to linq

--- cs/TagsCloudVisualization/Program.cs
+++ cs/TagsCloudVisualization/Program.cs
@@ -9,24 +9,21 @@ namespace TagsCloudVisualization;
 [SupportedOSPlatform("windows")]
 public static class Program
 {
-    public const int ImageWidth = 1600;
-    public const int ImageHeight = 1600;
-    public const int CountRectangles = 1000;
-    public const int minLength = 10;
-    public const int maxLength = 20;
+    private const int ImageWidth = 1600;
+    private const int ImageHeight = 1600;
+    private const int CountRectangles = 1000;
+    private const int MinLength = 10;
+    private const int MaxLength = 20;
     
     public static void Main()
     {
         var center = new Point(ImageWidth / 2, ImageHeight / 2);
         var cloudLayouter = new CircularCloudLayouter(center, 1, 0.5);
         var random = new Random();
-        var rectangles = new Rectangle[CountRectangles];
-        
-        for (int i = 0; i < CountRectangles; i++)
-        {
-            var size = random.NextSize(minLength, maxLength);
-            rectangles[i] = cloudLayouter.PutNextRectangle(size);
-        }
+        var rectangles = Enumerable
+            .Range(0, CountRectangles)
+            .Select(x => random.NextSize(MinLength, MaxLength))
+            .Select(size => cloudLayouter.PutNextRectangle(size));
 
         var visualizer = new DefaultVisualizer();
         var bitmap = visualizer.CreateBitmap(rectangles, new Size(ImageWidth, ImageHeight));
@@ -37,7 +34,7 @@ public static class Program
 
     private static string GetPathToTagsCloudImage()
     {
-        var fileName = $"{CountRectangles}_{minLength}_{maxLength}_TagsCloud.png";
+        var fileName = $"{CountRectangles}_{MinLength}_{MaxLength}_TagsCloud.png";
         var imagesDirectory = "images";
         Directory.CreateDirectory(imagesDirectory);
         
