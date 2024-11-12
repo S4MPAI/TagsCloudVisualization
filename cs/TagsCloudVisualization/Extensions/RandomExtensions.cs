b4a37d314a5f6b9e19be7a77549c83211722ab01
S4MPAI <flekshum@gmail.com>
2024-11-12 15:36:26 +0500

    RandomExtensions.cs: fix method NextSize

--- cs/TagsCloudVisualization/Extensions/RandomExtensions.cs
+++ cs/TagsCloudVisualization/Extensions/RandomExtensions.cs
@@ -6,6 +6,9 @@ public static class RandomExtensions
 {
     public static Size NextSize(this Random random, int minLength, int maxLength)
     {
+        if (minLength <= 0)
+            throw new ArgumentOutOfRangeException(nameof(minLength), minLength, "minLength must be greater than zero.");
+        
         var width = random.Next(minLength, maxLength);
         var height = random.Next(minLength, maxLength);
         
