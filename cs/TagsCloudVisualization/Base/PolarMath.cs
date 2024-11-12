ec2fa0e810af925e8217da25d4358648ef88daed
S4MPAI <flekshum@gmail.com>
2024-11-12 19:03:17 +0500

    add new methods in PolarMath

--- cs/TagsCloudVisualization/Base/PolarMath.cs
+++ cs/TagsCloudVisualization/Base/PolarMath.cs
@@ -7,6 +7,9 @@ public static class PolarMath
     public static double ConvertToRadians(double degrees) =>
         degrees * Math.PI / 180;
 
+    public static double ConvertToDegrees(double radians) =>
+        radians * 180 / Math.PI;
+    
     public static double GetOffsetPerRadianForArchimedeanSpiral(double radius) =>
         radius / (2 * Math.PI);
 
@@ -16,5 +19,14 @@ public static class PolarMath
         var y = (int)Math.Round(polarRadius * Math.Sin(polarAngle));
         
         return new Point(x, y);
+    }
+    
+    public static (double polarRadius, double polarAngle) ConvertToPolarCoordinateSystem(Point point)
+    {
+        var polarRadius = Math.Sqrt(point.X * point.X + point.Y * point.Y);
+        var polarAngle = Math.Atan2(point.Y, point.X);
+        polarAngle = polarAngle < 0 ? 2 * Math.PI + polarAngle : polarAngle;
+        
+        return (polarRadius, polarAngle);
     }
 }
