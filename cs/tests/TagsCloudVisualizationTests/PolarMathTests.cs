a1763b1ccf6b71d2ab56974a834b488c532d3509
S4MPAI <flekshum@gmail.com>
2024-11-12 19:05:37 +0500

    add tests for new methods PolarMath

--- cs/tests/TagsCloudVisualizationTests/PolarMathTests.cs
+++ cs/tests/TagsCloudVisualizationTests/PolarMathTests.cs
@@ -17,6 +17,17 @@ public class PolarMathTests
     [TestCase(360, ExpectedResult = 2 * Math.PI, TestName = "DegreesEqual360")]
     public double ConvertToRadians_ShouldReturnExpectedResult(double degrees) => 
         PolarMath.ConvertToRadians(degrees);
+    
+    [TestCase(0, ExpectedResult = 0, TestName = "RadiansEqualZero")]
+    [TestCase(Math.PI / 6, ExpectedResult = 30, TestName = "RadiansEqualOneSixthPi")]
+    [TestCase(Math.PI / 2, ExpectedResult = 90, TestName = "RadiansEqualHalfPi")]
+    [TestCase(2 * Math.PI / 3, ExpectedResult = 120, TestName = "RadiansEqualTwoThirdPi")]
+    [TestCase(Math.PI, ExpectedResult = 180, TestName = "RadiansEqualPi")]
+    [TestCase(3 * Math.PI / 2, ExpectedResult = 270, TestName = "RadiansEqualThreeSecondPi")]
+    [TestCase(7 * Math.PI / 4, ExpectedResult = 315, TestName = "RadiansEqualSevenFourthPi")]
+    [TestCase(2 * Math.PI, ExpectedResult = 360, TestName = "RadiansEqualTwoPi")]
+    public double ConvertToDegrees_ShouldReturnExpectedResult(double radians) => 
+        Math.Round(PolarMath.ConvertToDegrees(radians));
 
     [TestCase( 0.5 * Math.PI, ExpectedResult = 0.25, TestName = "RadiusEqualHalfOfPi")]
     [TestCase(Math.PI, ExpectedResult = 0.5, TestName = "RadiusEqualPi")]
@@ -25,15 +36,28 @@ public class PolarMathTests
     public double GetOffsetPerRadianForArchimedeanSpiral_ShouldReturnExpectedResult(double radius) => 
         PolarMath.GetOffsetPerRadianForArchimedeanSpiral(radius);
     
-    [TestCaseSource(nameof(_getOffsetPerRadianForArchimedeanSpiralTestCases))]
+    [TestCaseSource(nameof(_convertToCartesianCoordinateSystemTestCases))]
     public Point ConvertToCartesianCoordinateSystem_ShouldReturnExpectedResult(double polarAngle) => 
         PolarMath.ConvertToCartesianCoordinateSystem(10, polarAngle);
 
-    private static object[] _getOffsetPerRadianForArchimedeanSpiralTestCases =
+    private static object[] _convertToCartesianCoordinateSystemTestCases =
     {
         new TestCaseData(Math.PI / 2).Returns(new Point(0, 10)).SetName("PolarAngleEqualHalfOfPi"),
         new TestCaseData(Math.PI).Returns(new Point(-10, 0)).SetName("PolarAngleEqualPi"),
         new TestCaseData(0.25 * Math.PI).Returns(new Point(7, 7)).SetName("PolarAngleEqualQuarterOfPi"),
         new TestCaseData(1.5 * Math.PI).Returns(new Point(0, -10)).SetName("PolarAngleEqualHalfOfPi"),
+    };
+    
+    [TestCaseSource(nameof(_convertToPolarCoordinateSystemTestCases))]
+    public (double polarRadius, double polarAngle) ConvertToPolarCoordinateSystem_ShouldReturnExpectedResult(Point point) => 
+        PolarMath.ConvertToPolarCoordinateSystem(point);
+    
+    private static object[] _convertToPolarCoordinateSystemTestCases =
+    {
+        new TestCaseData(new Point(0, 0)).Returns((0, 0)),
+        new TestCaseData(new Point(1, 1)).Returns((Math.Sqrt(2), Math.PI / 4)),
+        new TestCaseData(new Point(-1, -1)).Returns((Math.Sqrt(2), 5 * Math.PI / 4)),
+        new TestCaseData(new Point(0, 2)).Returns((2, Math.PI / 2)),
+        new TestCaseData(new Point(-2, 0)).Returns((2, Math.PI))
     };
 }
