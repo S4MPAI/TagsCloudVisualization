ed3dd98d7ed4b7cceb44571d74564821c4d3b065
S4MPAI <flekshum@gmail.com>
2024-11-13 15:55:11 +0500

    CircularCloudLayouterTests.cs: add asserts on rectangles in circle

--- cs/tests/TagsCloudVisualizationTests/CircularCloudLayouterTests.cs
+++ cs/tests/TagsCloudVisualizationTests/CircularCloudLayouterTests.cs
@@ -2,6 +2,7 @@ using System.Drawing;
 using FluentAssertions;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
+using TagsCloudVisualization.Base;
 using TagsCloudVisualization.CloudLayouters;
 using TagsCloudVisualization.Extensions;
 
@@ -20,7 +21,7 @@ public class CircularCloudLayouterTests
     }
     
     [Test]
-    public void PutNextRectangle_ShouldReturnRectangleInCenter_IsFirstRectangle()
+    public void PutNextRectangle_ShouldReturnRectangleInCenter_FirstRectangle()
     {
         var size = new Size(100, 100);
         var circularCloudLayouter = new CircularCloudLayouter(new Point(0, 0), 1, 0.5);
@@ -30,20 +31,59 @@ public class CircularCloudLayouterTests
         
         actualRectangle.Should().BeEquivalentTo(expectedRectangle);
     }
+    
+    [Test]
+    [Repeat(10)]
+    public void PutNextRectangle_ShouldReturnRectanglesInCenter()
+    {
+        var center = random.NextPoint(-100, 100);
+        var rectangles = PutRectanglesInCloudLayouter(center);
 
+        var actualRectanglesWindow = new RectanglesWindow(rectangles);
+        var centerOffset = center.Subtract(actualRectanglesWindow.Center).Abs();
+        var xError = rectangles.Max(r => r.Width);
+        var yError = rectangles.Max(r => r.Height);
+        
+        centerOffset.X.Should().BeLessThanOrEqualTo(xError);
+        centerOffset.Y.Should().BeLessThanOrEqualTo(yError);
+    }
+    
+    [Test]
+    [Repeat(10)]
+    public void PutNextRectangle_ShouldReturnRectanglesInCircle()
+    {
+        var center = random.NextPoint(-100, 100);
+        var rectangles = PutRectanglesInCloudLayouter(center);
+        
+        var actualRectanglesWindow = new RectanglesWindow(rectangles);
+        var radius = (actualRectanglesWindow.Width + actualRectanglesWindow.Height) / 4;
+        var actualSquare = (double)rectangles.Sum(r => r.Width * r.Height);
+        var expectedSquare = PolarMath.GetSquareOfCircle(radius);
+
+        var precision = expectedSquare * 0.275;
+        actualSquare.Should().BeApproximately(expectedSquare, precision);
+    }
+    
     [Test]
     [Repeat(10)]
     public void PutNextRectangle_ShouldReturnDontIntersectsRectangles()
     {
+        var center = random.NextPoint(-100, 100);
+        var rectangles = PutRectanglesInCloudLayouter(center);
+        
+        IsHaveIntersects(rectangles).Should().BeFalse();
+    }
+    
+    private Rectangle[] PutRectanglesInCloudLayouter(Point center)
+    {
         var rectanglesCount = random.Next(100, 500);
-        var circularCloudLayouter = new CircularCloudLayouter(new Point(0, 0), 1, 0.5);
-        var rectangles = Enumerable
+        var circularCloudLayouter = new CircularCloudLayouter(center, 1, 0.5);
+
+        return Enumerable
             .Range(0, rectanglesCount)
             .Select(_ => random.NextSize(10, 50))
             .Select(s => circularCloudLayouter.PutNextRectangle(s))
             .ToArray();
-        
-        IsHaveIntersects(rectangles).Should().BeFalse();
     }
 
     private static bool IsHaveIntersects(Rectangle[] rectangles)
