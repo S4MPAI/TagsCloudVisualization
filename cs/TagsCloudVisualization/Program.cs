3e046c7e0ab92eed81762e3407021d55f276ff41
S4MPAI <flekshum@gmail.com>
2024-11-15 15:37:06 +0500

    update Program.cs on changed classes

--- cs/TagsCloudVisualization/Program.cs
+++ cs/TagsCloudVisualization/Program.cs
@@ -12,32 +12,27 @@ public static class Program
     private const int CountRectangles = 100;
     private const int MinLength = 30;
     private const int MaxLength = 80;
+    private const string ImagesDirectory = "images";
+    private static readonly Random Random = new();
     
     public static void Main()
     {
         var cloudLayouter = new CircularCloudLayouter(new Point(), 1, 0.1);
-        var random = new Random();
         var rectangles = Enumerable
             .Range(0, CountRectangles)
-            .Select(x => random.NextSize(MinLength, MaxLength))
+            .Select(_ => Random.NextSize(MinLength, MaxLength))
             .Select(size => cloudLayouter.PutNextRectangle(size))
             .ToList();
 
         var rectanglesWindow = new RectanglesWindow(rectangles);
-        var center = new Point(rectanglesWindow.Width / 2, rectanglesWindow.Height / 2);
-        var visualizer = new DefaultVisualizer(center);
-        using var bitmap = visualizer.CreateBitmap(rectangles, new Size(rectanglesWindow.Width, rectanglesWindow.Height));
+        var visualizer = new CartesianVisualizer(new Size(rectanglesWindow.Width, rectanglesWindow.Height));
+        using var bitmap = visualizer.CreateBitmap(rectangles);
+        
+        Directory.CreateDirectory(ImagesDirectory);
         var path = GetPathToTagsCloudImage();
-
         bitmap.Save(path);
     }
 
-    private static string GetPathToTagsCloudImage()
-    {
-        var fileName = $"{CountRectangles}_{MinLength}_{MaxLength}_TagsCloud.png";
-        var imagesDirectory = "images";
-        Directory.CreateDirectory(imagesDirectory);
-        
-        return $"{imagesDirectory}\\{fileName}";
-    }
+    private static string GetPathToTagsCloudImage() => 
+        Path.Combine(ImagesDirectory, $"{CountRectangles}_{MinLength}_{MaxLength}_TagsCloud.png");
 }
