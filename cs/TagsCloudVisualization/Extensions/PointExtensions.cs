53da26cfd3a7ca4259b00383230e46c0a627407e
S4MPAI <flekshum@gmail.com>
2024-11-15 15:35:26 +0500

    rename parameters in PointExtensions.cs

--- cs/TagsCloudVisualization/Extensions/PointExtensions.cs
+++ cs/TagsCloudVisualization/Extensions/PointExtensions.cs
@@ -4,9 +4,9 @@ namespace TagsCloudVisualization.Extensions;
 
 public static class PointExtensions
 {
-    public static Point Subtract(this Point p1, Point p2) => 
-        new(p1.X - p2.X, p1.Y - p2.Y);
+    public static Point Subtract(this Point point1, Point point2) => 
+        new(point1.X - point2.X, point1.Y - point2.Y);
     
-    public static Point Abs(this Point p) => 
-        new(Math.Abs(p.X), Math.Abs(p.Y));
+    public static Point Abs(this Point point) => 
+        new(Math.Abs(point.X), Math.Abs(point.Y));
 }
